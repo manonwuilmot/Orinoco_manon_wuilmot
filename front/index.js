126051081684ae86fffb839058a9cab24f75fd05
Wuilmot Manon <manonwuilmot7@gmail.com>
2021-07-16 11:52:28 +0200

    optimisation index

--- front/index.js
+++ front/index.js
@@ -8,86 +8,67 @@ function displayAllProducts() {
     .then(function (data) {
       //fonction pour créer le template de base d'une fiche produit//
       function templateProduct() {
-        const main = document.querySelector("main");
-        const productSheet = document.createElement("article");
-        main.appendChild(productSheet);
-        productSheet.classList.add("product-sheet");
+        const article = document.createElement("article");
+        document.querySelector("main").appendChild(article);
+        article.classList.add("product-sheet");
+
+        const figure = document.createElement("figure");
+        article.appendChild(figure);
+        figure.classList.add("product-sheet-photo");
+
+        const productLink = document.createElement("a");
+        figure.appendChild(productLink);
+
+        const img = document.createElement("img");
+        productLink.appendChild(img);
+        img.classList.add("product-sheet-photo-img");
+
+        const figcaption = document.createElement("figcaption");
+        figure.appendChild(figcaption);
+        figcaption.classList.add("product-sheet-description");
+
+        const title = document.createElement("div");
+        figcaption.appendChild(title);
+        title.classList.add("product-sheet-description-title");
+
+        const description = document.createElement("p");
+        figcaption.appendChild(description);
+        description.classList.add("product-sheet-description-text");
+
+        const nameOfProduct = document.createElement("div");
+        title.appendChild(nameOfProduct);
+        nameOfProduct.classList.add("product-sheet-description-title-name");
+
+        const priceOfProduct = document.createElement("div");
+        title.appendChild(priceOfProduct);
+        priceOfProduct.classList.add("product-sheet-description-title-price");
       }
       //boucle pour afficher tous les produits présents dans l'API//
       for (let i = 0; i < data.length; i++) {
-        templateProduct(data[i]);
+        //afficher un template pour chaque produit présent dans l'API//
+        templateProduct();
 
-        //création des éléments du DOM //
-        const articles = document.querySelectorAll("article");
-        const productSheetImage = document.createElement("figure");
-        articles[i].appendChild(productSheetImage);
-        productSheetImage.classList.add("product-sheet-photo");
-
-        document.querySelector("figure");
-        const productSheetDescription = document.createElement("figcaption");
-        articles[i].appendChild(productSheetDescription);
-        productSheetDescription.classList.add("product-sheet-description");
-        const figcaption = document.querySelectorAll("figcaption");
-
-        //création élément title contenant le nom et le prix//
-        const productSheetDescriptionTitle = document.createElement("div");
-        figcaption[i].appendChild(productSheetDescriptionTitle);
-        productSheetDescriptionTitle.classList.add(
-          "product-sheet-description-title"
-        );
-        //création élément description de l'ours en peluche//
-        const productSheetDescriptionText = document.createElement("p");
-        figcaption[i].appendChild(productSheetDescriptionText);
-        productSheetDescriptionText.classList.add(
-          "product-sheet-description-text"
-        );
-        //affichage données descriptions//
+        // //affichage description//
         const descriptions = document.querySelectorAll(
           ".product-sheet-description-text"
         );
         descriptions[i].innerHTML = data[i].description;
-
-        const divTitle = document.querySelectorAll(
-          "div.product-sheet-description-title"
-        );
-
-        //création élément nom de l'ours en peluche//
-        const productSheetDescriptionTitleName = document.createElement("div");
-        divTitle[i].appendChild(productSheetDescriptionTitleName);
-        productSheetDescriptionTitleName.classList.add(
-          "product-sheet-description-title-name"
-        );
-        //affichage nom//
-        const names = document.querySelectorAll(
+        // //affichage nom//
+        let names = document.querySelectorAll(
           ".product-sheet-description-title-name"
         );
-        names[i].innerHTML = data[i].name;
-
-        //création élément prix de l'ours en peluche//
-        const productSheetDescriptionTitlePrice = document.createElement("div");
-        divTitle[i].appendChild(productSheetDescriptionTitlePrice);
-        productSheetDescriptionTitlePrice.classList.add(
-          "product-sheet-description-title-price"
-        );
-        //affichage prix//
+        names[i].textContent = data[i].name;
+        // //affichage prix//
         const prices = document.querySelectorAll(
           ".product-sheet-description-title-price"
         );
         prices[i].innerHTML = data[i].price / 100 + " euros";
 
-        //création élément image et lien de l'ours en peluche//
-        const figures = document.querySelectorAll("figure");
-
-        const productLink = document.createElement("a");
-        figures[i].appendChild(productLink);
-        productLink.classList.add("product-sheet-photo-img");
-        productLink.href = "product.html" + "?id=" + data[i]._id;
-
-        const links = document.querySelectorAll("figure a");
-        const img = document.createElement("img");
-        links[i].appendChild(img);
-        img.classList.add("product-sheet-photo-img");
-        img.src = data[i].imageUrl;
+        // //affichage image et lien pour rediriger sur la page produit//
+        const productLinks = document.querySelectorAll("figure a");
+        productLinks[i].href = "product.html" + "?id=" + data[i]._id;
+        const images = document.querySelectorAll("img.product-sheet-photo-img");
+        images[i].src = data[i].imageUrl;
       }
     });
 }
