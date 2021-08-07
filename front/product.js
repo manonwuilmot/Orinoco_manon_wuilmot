a59a21b5d16ff43bcfc982efd0f925c107b186a6
Wuilmot Manon <manonwuilmot7@gmail.com>
2021-08-07 17:26:09 +0200

    optimisation

--- front/product.js
+++ front/product.js
@@ -2,55 +2,59 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 
-//fonction pour créer un template dynamique pour la page produit //
-function templateOneProduct() {
-  const main = document.querySelector("main");
-  main.classList.add("main");
-  main.innerHTML =
-    '<div class="pop-up-confirmation"></div><article class="page-product-sheet"><figure class="page-product-sheet-photo"><img class="page-product-sheet-photo-img"></figure><div class="page-product-sheet-description"><div class="page-product-sheet-description-title"><div class="page-product-sheet-description-title-name"></div><div class="page-product-sheet-description-title-price"></div></div><p class="page-product-sheet-description-text"></p></div><div class="product-colors">Disponible dans les coloris suivants : <select class="colors"><option placeholder="choisir un pelage">Choisir un pelage</option></select></div></article';
-
-  //ajout d'un bouton pour ajouter le produit au panier//
-  const inputAddToBag = document.createElement("button");
-  const article = document.querySelector("article");
-  article.appendChild(inputAddToBag);
-  inputAddToBag.setAttribute("id", "btn-add");
-  inputAddToBag.setAttribute("type", "button");
-}
-//fonction pour les options couleurs//
-function templateOptionsColors() {
-  //ajout des options couleurs//
-  const optionColor = document.createElement("option");
-  document.querySelector(".colors").appendChild(optionColor);
-  optionColor.classList.add("option-color");
-}
-
 // Récupération des données du produit//
 fetch("http://localhost:3000/api/teddies/" + id)
-  .then((response) => response.json())
+  .then((res) => res.json())
 
   .then(function (data) {
-    //récupération du template de la page produit//
-    templateOneProduct();
+    //création d'un template dynamique pour la page produit et récupération des données de l'API//
 
-    //affichages des données provenant de l'API//
-    document.querySelector(".page-product-sheet-photo-img").src = data.imageUrl;
-    //affichage bouton avec prix dynamique//
-    document.getElementById("btn-add").textContent =
-      "Ajouter au panier pour " + data.price / 100 + " euros";
-    //affichage de la description du produit//
-    document.querySelector(".page-product-sheet-description-text").innerHTML =
-      data.description;
-    document.querySelector(
-      ".page-product-sheet-description-title-name"
-    ).innerHTML = data.name;
-    //affichage du prix//
-    document.querySelector(
-      ".page-product-sheet-description-title-price"
-    ).innerHTML = data.price / 100 + " euros";
+    const main = document.querySelector("main");
+    main.innerHTML = `
+      <div class="pop-up-confirmation">Merci beaucoup ! L'ours en peluche
+        ${data.name} a bien été ajouté au panier !
+        <a href="order.html" class="pop-up-links">
+          <button id="gotobag">Passer ma commande
+        </a>
+          </button>
+        <button id="continueshopping">Continuer mon shopping</button>
+      </div>
+      <article class="page-product-sheet">
+        <figure class="page-product-sheet-photo">
+          <img src=${data.imageUrl} class="page-product-sheet-photo-img"/>
+        </figure>
+        <div class="page-product-sheet-description">
+          <div class="page-product-sheet-description-title">
+            <div class="page-product-sheet-description-title-name">${
+              data.name
+            }</div>
+            <div class="page-product-sheet-description-title-price">${
+              data.price / 100
+            } €</div>
+          </div>
+          <p class="page-product-sheet-description-text">${data.description}</p>
+        </div>
+        <div class="product-colors">
+          Disponible dans les coloris suivants :
+          <select class="colors">
+            <option placeholder="choisir un pelage">Choisir un pelage</option>
+          </select>
+        </div>
+        <button id="btn-add" type="button">Ajouter au panier pour ${
+          data.price / 100
+        } euros</button>
+      </article>`;
+
+    //fonction pour les options couleurs//
+    function templateOptionsColors() {
+      const optionColor = document.createElement("option");
+      document.querySelector(".colors").appendChild(optionColor);
+      optionColor.classList.add("option-color");
+    }
 
     //Boucle pour affichage des options couleurs//
     for (let i = 0; i < data.colors.length; i++) {
-      templateOptionsColors(data[i]);
+      templateOptionsColors();
       const options = document.querySelectorAll(".option-color");
       options[i].innerHTML = data.colors[i];
     }
@@ -59,28 +63,14 @@ fetch("http://localhost:3000/api/teddies/" + id)
     const inputAddToBag = document.getElementById("btn-add");
 
     inputAddToBag.addEventListener("click", () => {
-      //fenêtre qui s'affiche pour indiquer que le produit a bien été ajouté//
+      //animation de la fenêtre pop-up de confirmation d'ajout au panier//
       const popupConfirmation = document.querySelector(".pop-up-confirmation");
-      popupConfirmation.textContent =
-        "Merci beaucoup ! L'ours en peluche " +
-        data.name +
-        " a bien été ajouté au panier !";
-
-      //animation de la fenêtre//
       popupConfirmation.animate([{ opacity: "1" }], {
         duration: 1000,
         fill: "forwards",
       });
-      //création de deux boutons, l'un pour aller au panier, l'autre pour retourner au shopping//
-      const buttonGoToBag = document.createElement("button");
-      popupConfirmation.appendChild(buttonGoToBag);
-      buttonGoToBag.setAttribute("id", "gotobag");
-      buttonGoToBag.innerHTML =
-        '<a href="order.html" class="pop-up-links">Passer ma commande</a>';
-      const buttonContinueShopping = document.createElement("button");
-      popupConfirmation.appendChild(buttonContinueShopping);
-      buttonContinueShopping.textContent = "Continuer mon shopping";
-      buttonContinueShopping.setAttribute("id", "continueshopping");
+      const buttonContinueShopping =
+        document.getElementById("continueshopping");
       buttonContinueShopping.addEventListener("click", () => {
         popupConfirmation.animate([{ opacity: "0" }], {
           duration: 1000,
@@ -88,30 +78,20 @@ fetch("http://localhost:3000/api/teddies/" + id)
         });
       });
 
-      // // // création nouvel objet, récupération contenu du produit et traduction de javascript en json//
+      // // création nouvel objet, récupération contenu du produit et traduction de javascript en json//
 
-      const imgOfProduct = document.querySelector(
-        ".page-product-sheet-photo-img"
-      ).src;
-      const priceOfProduct = data.price / 100;
-      const id = data._id;
-
-      let quantity = 1;
-
-      // créer un nouveau produit
       let product = {
-        id: id,
+        id: data._id,
         name: data.name,
-        price: priceOfProduct,
-        quantity: quantity,
-        img: imgOfProduct,
+        price: data.price / 100,
+        quantity: 1,
+        img: data.img,
       };
 
       let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
       //fonction pour initialiser le panier et ajouter un nouveau produit//
       function addProduct() {
-        let cart = JSON.parse(localStorage.getItem("cart") || "[]");
         cart.push(product);
         localStorage.setItem("cart", JSON.stringify(cart));
       }
